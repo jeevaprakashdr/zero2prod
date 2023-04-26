9a1ba77a2b2ba8625ee7e3180c02d36ebef46a33
jeeva <jeevaprakashdr@hotmail.com>
2023-04-26 22:02:08 +0100

    Enable logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -12,5 +13,6 @@ async fn main() -> Result<(), std::io::Error> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     run(listener, connection_pool)?.await
 }
