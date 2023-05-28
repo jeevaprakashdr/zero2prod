f0180781a1c5811a50df0d53de0176c767edfeab
jeeva <jeeva.ramu@codurance.com>
2023-05-28 18:34:47 +0100

    false sense of safety net

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -21,10 +22,25 @@ pub async fn subscribe(
     subscribe_request: web::Form<SubscribeRequest>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    if !is_valid_name(&subscribe_request.name) {
+        return HttpResponse::BadRequest().finish();
+    }
+
     match insert_subscriber(&subscribe_request, &connection_pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
+}
+
+fn is_valid_name(name: &str) -> bool {
+    let is_empty_or_whitespace = name.trim().is_empty();
+
+    let is_too_long = name.grapheme_indices(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = name.chars().any(|c| forbidden_characters.contains(&c));
+    
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
 
 #[tracing::instrument(
