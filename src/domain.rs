5275c48f78599c7f0a0f720923545671ef2ff8b9
jeeva <jeeva.ramu@codurance.com>
2023-05-28 19:28:40 +0100

    imnplemented as_ref trait on subscriber name domain type

--- src/domain.rs
+++ src/domain.rs
@@ -7,11 +7,13 @@ pub struct NewSubscriber {
 
 pub struct SubscriberName(String);
 
-impl SubscriberName {
-    pub fn inner_ref(&self) -> &str {
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
         &self.0
     }
-    
+}
+
+impl SubscriberName {
     pub fn parse(s: String) -> SubscriberName {
         let is_empty_or_whitespace = s.trim().is_empty();
 
