7a06c6b1f35bac441846fe70bffc07cd2c657bca
jeeva <jeeva.ramu@codurance.com>
2023-06-21 13:00:44 +0200

    added reqwest client

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,13 +1,24 @@
 use crate::domain::SubscriberEmail;
+use reqwest::Client;
 
 pub struct EmailClient {
     sender: SubscriberEmail,
+    http_client: Client,
+    base_url: String,
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            sender,
+            http_client: Client::new(),
+            base_url,
+        }
+    }
+
     pub async fn send_email(
         &self,
-        recipent: SubscriberEmail,
+        recipient: SubscriberEmail,
         subject: &str,
         html_content: &str,
         text_content: &str,
