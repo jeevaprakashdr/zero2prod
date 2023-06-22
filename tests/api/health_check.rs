663450c01e9b0833c0ea926dbaa2f0395807f143
jeeva <jeeva.ramu@codurance.com>
2023-06-22 15:46:52 +0200

    restructure tests

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,12 +1,5 @@
+use crate::helpers::spawn_app;
 use reqwest::Client;
-use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::{net::TcpListener, time::Duration};
-use uuid::Uuid;
-use zero2prod::{
-    configuration::{get_configuration, DatabaseSettings},
-    email_client::EmailClient,
-    startup::run,
-};
 
 #[tokio::test]
 async fn should_return_ok_response() {
@@ -114,59 +107,4 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         );
     }
-}
-
-pub struct TestApp {
-    pub address: String,
-    pub db_pool: PgPool,
-}
-
-async fn spawn_app() -> TestApp {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid email sender");
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        Duration::from_secs(configuration.email_client.timeout_milliseconds),
-    );
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-    TestApp {
-        address,
-        db_pool: connection_pool,
-    }
-}
-
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect_with(&config.without_db())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
-        .await
-        .expect("Failed to create database.");
-
-    let connection_pool = PgPool::connect_with(config.with_db())
-        .await
-        .expect("Failed to connect to Postgres.");
-
-    sqlx::migrate!("./migrations")
-        .run(&connection_pool)
-        .await
-        .expect("Failed to migrate the database");
-
-    connection_pool
 }
